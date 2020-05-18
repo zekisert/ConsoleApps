4a6425ac0eae3a972ffc3ee8d59a7a098c1f7a13
zekisert <49846775+zekisert@users.noreply.github.com>
2020-05-18 22:11:33 +0300

    ProductManager.cs

---
+++ Methods/Boz Training/ProductManager.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class ProductManager
+    {
+        public void add()
+        {
+            Console.WriteLine("Product added");
+        }
+
+        public void update()
+        {
+            Console.WriteLine("Product updated");
+        }
+    }
+
+}
