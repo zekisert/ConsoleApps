c95c2746f6cba0c1c7144794e948c1999aa0bf2a
zekisert <49846775+zekisert@users.noreply.github.com>
2020-05-18 22:10:45 +0300

    CustomerManager.cs

---
+++ Methods/Boz Training/CustomerManager.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class CustomerManager
+    {
+        public void add()
+        {
+            Console.WriteLine("customer added");
+        }
+
+        public void update()
+        {
+            Console.WriteLine("customer updated");
+        }
+    }
+
+}
