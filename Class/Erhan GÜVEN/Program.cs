3e42d2491a261cf8d6e6a513b40be94ed5d1800f
zekisert <49846775+zekisert@users.noreply.github.com>
2020-05-19 18:35:29 +0300

    Update Program.cs

--- Class/Erhan GÜVEN/Program.cs
+++ Class/Erhan GÜVEN/Program.cs
@@ -10,49 +10,71 @@ namespace Siniflar
     {
         static void Main(string[] args)
         {
+            /*
             Musteriler musteri = new Musteriler();
             musteri.MusteriEkle("Erhan");
             musteri.MusteriSil("Erhan");
 
             Hesaplar hesap = new Hesaplar();
             hesap.HesapEkle("Erhan", 500);
-            
-       /*
-            //Değer Tip Ataması
+            */
+
+            /*
+            Değer Tip Ataması
             int a = 50;
             int b = a;
             b = 30;
             Console.WriteLine(a);
             Console.WriteLine(b);
 
-            //Referans Tip Ataması
+            Referans Tip Ataması
             Musteriler muster1 = new Musteriler();
             Musteriler musteri2 = muster1;
             musteri2.MusteriIsmi = "Erhan";
             Console.WriteLine(muster1.MusteriIsmi);
             Console.WriteLine(musteri2.MusteriIsmi);
-       */
-            
-       /*
+             */
+
+            /*
             StaticYapilar statikyapilar = new StaticYapilar();
             statikyapilar.StatikOlmayanIslemYap();
             StaticYapilar.StatikOlanIslemYap();
-       */
-            Hesaplar hesap = new Hesaplar();
-            
-             /*    
+            */
+
+            //Hesaplar hesap = new Hesaplar(50, "Erhan");
+
+            /*    
             Ozellikler ozellik = new Ozellikler();
             ozellik.Sayi = 3;
             Console.WriteLine(ozellik.Sayi);
             */
 
+            /*
+            Console.WriteLine(Sabitler.pi);
+            Sabitler sabit = new Sabitler();
+            Console.WriteLine(sabit.Sayi);
+            */
+
+            /*
+            Hesaplar hesap = new Hesaplar(50,"Erhan");
+            hesap.HesapEkle();
+            Hesaplar hesap2 = new Hesaplar(60, "Toprak");
+            hesap2.HesapEkle();
+            */
+
+
+
+
+
+
             Console.ReadLine();
         }
 
-       
+
     }
 
-   
 
-    
+
+
 }
+
