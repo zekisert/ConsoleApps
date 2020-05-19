f955d1feb822ca1b2ee5221837643d8137d62117
zekisert <49846775+zekisert@users.noreply.github.com>
2020-05-19 18:34:11 +0300

    Update Hesaplar.cs

--- Class/Erhan GÜVEN/Hesaplar.cs
+++ Class/Erhan GÜVEN/Hesaplar.cs
@@ -8,19 +8,40 @@ namespace Siniflar
 {
     public class Hesaplar
     {
+        private int _sayi;
+        private string _isim;
         public Hesaplar(int sayi,string isim)
         {
             Console.WriteLine($"{sayi} {isim}  Constructor Metodu Çalıştırıldı.");
+            _sayi = sayi;
+            _isim = isim;
         }
-        
+
+        /*
         public void HesapEkle(string MusteriAdi, int hesapNo)
         {
             Console.WriteLine($"{MusteriAdi} adlı müşterinin {hesapNo} hesabı eklendi.");
         }
+        */
 
+        public void HesapEkle()
+        {
+            Console.WriteLine($"{_isim} adlı müşterinin {_sayi} hesabı eklendi.");
+        }
+
+        /*
         public void HesapSil(string MusteriAdi, int hesapNo)
         {
             Console.WriteLine($"{MusteriAdi} adlı müşterinin {hesapNo} hesabı silindi.");
         }
+        */
+
+        
+        public void HesapSil()
+        {
+            Console.WriteLine($"{_isim} adlı müşterinin {_sayi} hesabı silindi.");
+        }
+        
     }
 }
+
