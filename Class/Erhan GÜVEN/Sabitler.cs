b6fce84abcbfab58fcdb8031683b88600cda3b14
zekisert <49846775+zekisert@users.noreply.github.com>
2020-05-19 16:57:00 +0300

    Sabitler.cs

---
+++ Class/Erhan GÜVEN/Sabitler.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siniflar
+{
+    class Sabitler
+    {
+        public const double pi = 3.14;
+        public readonly int Sayi = 5;
+    }
+}
